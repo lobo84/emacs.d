bcd8c08dcd0c4ac4f70fe6b59cf8b774f5fac6ea
Niclas Brandt <niclas.ericson@gmail.com>
2015-10-12 22:13:35 +0200

    company mode, irony

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (setq-default major-mode 'text-mode)
 
 ;; for tilde to work
-(require 'iso-transl) 
+(require 'iso-transl)
 
 (require 'package)
 ;; common lisp
@@ -20,12 +20,17 @@
     python
     rust-mode
     solarized-theme
-    zenburn-theme org)
+    zenburn-theme org
+    irony
+    company
+    company-irony
+    flycheck
+    flycheck-irony)
   )
 
 (defun my-packages-installed-p ()
   (loop for p in my-packages
-        when (not (package-installed-p p)) 
+        when (not (package-installed-p p))
 	do (return nil)
         finally (return t)))
 
@@ -39,34 +44,58 @@
     (when (not (package-installed-p p))
       (package-install p))))
 
-(load-theme 'zenburn t)
+;;(load-theme 'solarized t)
 (custom-set-faces
- '(default ((t (
-		:family "Droid Sans Mono" 
-			:foundry "unknown" 
-			:slant normal 
-			:weight normal 
-			:height 90 
-			:width normal)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 98 :width normal)))))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(irony-cdb-compilation-databases (quote (irony-cdb-json)))
  '(tool-bar-mode nil))
 
-
-;; Rename file
-(defun rename-file-and-buffer ()
-  "Rename the current buffer and file it is visiting."
-  (interactive)
-  (let ((filename (buffer-file-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (message "Buffer is not visiting a file!")
-      (let ((new-name (read-file-name "New name: " filename)))
-        (cond
-         ((vc-backend filename) (vc-rename-file filename new-name))
-         (t
-          (rename-file filename new-name t)
-          (set-visited-file-name new-name t t)))))))
-
-(global-set-key (kbd "C-c r")  'rename-file-and-buffer)
 (put 'narrow-to-region 'disabled nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; C++
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Irony mode for c++
+;; install libclang then run irony-install-server
+;; for cmake projectes generate a compilation database with
+;; -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+;; (defun my-irony-mode-hook ()
+;;   (define-key irony-mode-map [remap completion-at-point]
+;;     'irony-completion-at-point-async)
+;;   (define-key irony-mode-map [remap complete-symbol]
+;;     'irony-completion-at-point-async))
+;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; Add company-irony to your company backends.
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+;; Add company mode to all buffers
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Add flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+;;; init.el ends here
